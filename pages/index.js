19af42b6ff284bb77745b6a367c488578657caf7
garvit <garvitmaloo.21@gmail.com>
2023-01-24 12:06:07 +0530

    Basic setup

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next JS starting template</title>
+      </Head>
+      <main>
+        <h1>Welcome to Next JS</h1>
+      </main>
+    </>
+  );
+}
